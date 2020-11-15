76380edd7babccf1a35fd4cdf04be9202c5475e6
sgmjwbg <61115527+sgmjwbg@users.noreply.github.com>
2020-11-15 11:15:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -104,7 +104,7 @@ install_soga() {
         fi
     else
         last_version=$1
-        url="https://github.com/RManLuo/crack-soga-v2ray/releases/download/${last_version}/soga-cracked-linux64.tar.gz"
+        url="https://aaaa.hhkk.cf/${last_version}/soga.tar.gz"
         echo -e "开始安装 soga v$1"
         wget -N --no-check-certificate -O /usr/local/soga.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
